522793ada45f531522b342f9165e67a801e8ca6f
wjg50058 <jieguang.wang@ly.com>
2020-12-05 11:49:36 +0800

    年度账单

--- src/views/moonlight/yearbill/YearBill.vue
+++ src/views/moonlight/yearbill/YearBill.vue
@@ -1,12 +1,24 @@
 <template>
-  <swiper ref="mySwiper" :options="swiperOptions">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
-    <swiper-slide>Slide 5</swiper-slide>
-    <div class="swiper-pagination" slot="pagination"></div>
-  </swiper>
+  <div class="flex">
+    <swiper ref="mySwiper" :options="swiperOptions" :style="{ height: swiperHeight, width: swiperWidth }">
+      <swiper-slide>
+        <div class="bgimg1">fuck</div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="bgimg2">fuck</div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="bgimg3">fuck</div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="bgimg4">fuck</div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="bgimg5">fuck</div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -18,15 +30,109 @@ export default {
         pagination: {
           el: '.swiper-pagination'
         },
-        direction: 'vertical',
-        height: window.innerHeight
+        direction: 'vertical'
       }
     }
   },
   computed: {
     swiper () {
       return this.$refs.mySwiper.$swiper
+    },
+    swiperHeight () {
+      return window.innerHeight + 'px'
+    },
+    swiperWidth () {
+      return window.innerWidth + 'px'
     }
   }
 }
 </script>
+<style scoped>
+.flex {
+  /*flex 布局*/
+  display: flex;
+  /*实现垂直居中*/
+  align-items: center;
+  /*实现水平居中*/
+  justify-content: center;
+  text-align: justify;
+  max-width: 600px;
+  /*height: 200px; */
+  margin: 0 auto;
+}
+
+.bgimg1 {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background: url(/bill/bill1.jpg) no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+
+.bgimg2 {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background: url(/bill/bill2.jpg) no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+
+.bgimg3 {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background: url(/bill/bill3.jpg) no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+
+.bgimg4 {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background: url(/bill/bill4.jpg) no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+
+.bgimg5 {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background: url(/bill/bill5.png) no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+</style>
